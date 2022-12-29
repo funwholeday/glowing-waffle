94d1c9e51e01414ca1ff36c21ebcba36fb14d875
ygq <gqyoung233@163.com>
2022-12-29 14:56:26 +0800

    treeNode结构初始化

--- src/ExistsClass/TreeNode.java
+++ src/ExistsClass/TreeNode.java
@@ -5,7 +5,16 @@ public class TreeNode {
     public TreeNode left;
     public TreeNode right;
 
+    public static TreeNode root;
     public TreeNode(int x) {
         val = x;
+    }
+
+
+    public static TreeNode getTree() {
+        root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        return root;
     }
 }
